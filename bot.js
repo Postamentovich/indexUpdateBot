4025b0a1f569bc5fa76d265c67d2ed93e18cc42e
Viacheslav Zinovev <postamentovich@gmail.com>
2021-03-08 00:28:20 +0300

    init

---
+++ bot.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const { Telegraf } = require("telegraf");
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username;
+});
+const allowedWords = ["Денис", "Warehouse", "Олег", "Наберу"];
+
+const sendSticker = async (ctx) => {
+  const stickerSet = await ctx.getStickerSet("indexUpdate");
+  const stickers = stickerSet.stickers.map((sticker) => sticker.file_id);
+  const stickerId = Math.round(Math.random() * stickers.length);
+  const sticker = stickers[stickerId];
+  if (!sticker) return;
+  ctx.replyWithSticker(sticker);
+};
+
+const isShouldUpdated = () => {
+  return Math.random() < 0.3;
+};
+
+bot.start((ctx) => ctx.reply("Я вас переиграю"));
+bot.help((ctx) => ctx.reply("Даже не думай"));
+bot.on("text", async (ctx) => {
+  if (
+    allowedWords.some((word) =>
+      ctx.update.message.text.toLowerCase().match(word.toLowerCase())
+    ) ||
+    isShouldUpdated()
+  ) {
+    return sendSticker(ctx);
+  }
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
